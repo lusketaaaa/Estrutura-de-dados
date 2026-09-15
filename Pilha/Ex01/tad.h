149a79907f556a8d2c90ea7a19d957f8256b8422
lusketa <155599740+lusketaaaa@users.noreply.github.com>
2026-09-14 21:22:47 -0300

    Add basic stack operations in tad.h
    Implemented basic stack operations including initialization, push, pop, check full/empty, display, and concatenate stacks.

---
+++ Pilha/Ex01/tad.h
@@ -0,0 +1,44 @@
+#define MAX 20
+
+struct TpPilha {
+	int Topo;
+	int Pilha[MAX];
+};
+
+// tad basico da pilha
+void Inicializar(TpPilha &p) {
+	p.Topo = -1;
+}
+
+void Empilhar(TpPilha &p, int e) {
+	p.Pilha[++p.Topo] = e;
+}
+
+int Desempilhar(TpPilha &p) {
+	return p.Pilha[p.Topo--];
+}
+
+char Cheia(int topo) {
+	return topo == MAX-1;
+}
+
+char Vazia(int topo) {
+	return topo == -1;
+}
+
+void Exibir(TpPilha p) {
+	while(!Vazia(p.Topo)) 
+		printf("%d\n",Desempilhar(p));
+}
+
+void Concatenar(TpPilha &P1, TpPilha P2, TpPilha &P3) {
+	int num;
+	while(!Vazia(P2.Topo) && !Cheia(P3.Topo)) {
+		num = Desempilhar(P2);
+		Empilhar(P3, num);
+	}
+	while(!Vazia(P3.Topo) && !Cheia(P1.Topo)) {
+		num = Desempilhar(P3);
+		Empilhar(P1, num);
+	}
+}
